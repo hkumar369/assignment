a7209a2a00a93b3dfbd1cda8a95a91afc422ca34
hkumar369 <89292087+hkumar369@users.noreply.github.com>
2021-08-21 11:52:27 +0530

    Add files via upload

---
+++ project/app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Account_details,employ_details,task_details
+# Register your models here.
+admin.site.register(Account_details)
+admin.site.register(employ_details)
+admin.site.register(task_details)
